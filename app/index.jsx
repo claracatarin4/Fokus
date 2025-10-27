26394f34690f8278a66f1ade39b504df4ecc08fd
claracatarin4 <claracatarina47@gmail.com>
2025-10-27 09:48:46 -0300

    27/10/2025

--- app/index.jsx
+++ app/index.jsx
@@ -1,137 +1,37 @@
-import { useRef, useState } from "react";
-import { Image, StyleSheet, Text, View, Pressable } from "react-native";
-import { FokusButton } from "../components/FokusButton";
-import { ActionButton } from "../components/ActionButton";
-import { Timer } from "../components/Timer";
-import { IconPause, IconPlay } from "../components/Icons";
-
-const pomodoro = [
-  {
-    id: 'focus',
-    initialValue: 25 * 60,
-    image: require('./pomodoro.png'),
-    display: 'Foco'
-  },
-  {
-    id: 'short',
-    initialValue: 5 * 60,
-    image: require('./short.png'),
-    display: 'Pausa curta'
-  },
-  {
-    id: 'long',
-    initialValue: 15 * 60,
-    image: require('./long.png'),
-    display: 'Pausa longa'
-  },
-]
+import { View , Text, Image, StyleSheet} from 'react-native';
+import { FokusButton } from '../components/FokusButton';
 
 export default function Index() {
-
-  const [timerType, setTimerType] = useState(pomodoro[0])
-  const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
-  const [timerRunning, setTimerRunning] = useState(false)
-
-  const timerRef = useRef(null)
-
-  const clear = () => {
-    if (timerRef.current != null) {
-      clearInterval(timerRef.current)
-      timerRef.current = null
-      setTimerRunning(false)
-    }
-  }
-
-  const toggleTimerType = (newTimerType) => {
-    setTimerType(newTimerType)
-    setSeconds(newTimerType.initialValue)
-    clear()
-  }  
-
-  const toggleTimer = () => {
-    if (timerRef.current) {
-      clear()
-      return
-    }
-    
-    setTimerRunning(true)
-
-    const id = setInterval(() => {
-      setSeconds(oldState => {
-        if (oldState === 0) {
-          clear()
-          return timerType.initialValue
-        }
-        return oldState - 1
-      })
-    }, 1000)
-    timerRef.current = id
-  }
-
-  return (
-    <View
-      style={styles.container}
-    >
-      <Image source={timerType.image}/>
-      <View style={styles.actions}>
-        <View style={styles.context}>
-          {pomodoro.map(p => (
-            <ActionButton 
-              key={p.id}
-              active={ timerType.id === p.id }
-              onPress={() => toggleTimerType(p)}
-              display={p.display}
-            />
-          ))}
-        </View>
-        <Timer totalSeconds={seconds} />
-        <FokusButton 
-          title={timerRunning ? 'Pausar' : 'Começar'}
-          icon={timerRunning ? <IconPause /> : <IconPlay />}
-          onPress={toggleTimer}
-        />
-      </View>
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>
-          Projeto fictício e sem fins comerciais.
-        </Text>
-        <Text style={styles.footerText}>
-          Desenvolvido por Aluno. 
-        </Text>
-      </View>
+ return (
+   <View>
+    <Image source={require('../assets/images/logo.png')} style={{ width: 200, height: 200 }} />
+    <View>
+      <Text> 
+        Otimize sua produtividade, 
+        mergulhe no que importa
+      </Text>
+      <Image source={require('../assets/images/tela_inicial.png')} style={{ width: 300, height: 200 }} />
+      <FokusButton title='Quero Iniciar!'/>
     </View>
+  
+   </View>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: '#021123',
-    gap: 40
-  },
-  actions: {
-    paddingVertical: 24,
-    paddingHorizontal: 24,
-    backgroundColor: '#14448080',
-    width: '80%',
+  button: {
+    backgroundColor: '#B872FF',
     borderRadius: 32,
-    borderWidth: 2,
-    borderColor: '#144480',
-    gap: 32
-  },
-  context: {
+    padding: 8,
     flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    gap: 12,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  footer: {
-    width: '80%',
-  },
-  footerText: {
+  buttonText: {
     textAlign: 'center',
-    color: '#98A0A8',
-    fontSize: 12.5
+    color: '#021123',
+    fontSize: 18
   }
 })
